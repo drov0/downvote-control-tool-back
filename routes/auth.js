9ef4509a6cd8fc40086f40a81d246bfc701daa10
Martin Lees <drov0@users.noreply.github.com>
2019-09-08 13:36:25 +0200

    minimum payout done

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,7 @@ router.post('/user',urlencodedParser, async function(req, res, next) {
 
             data = data[0];
 
-            return res.send({status : "ok", threshold : data.threshold});
+            return res.send({status : "ok", threshold : data.threshold, min_payout : data.min_payout});
         } else
             return res.send({status : "ko"});
     }
@@ -76,8 +76,10 @@ router.get('/conf',async function(req, res, next) {
             if (data.length === 0) {
                 await db("INSERT INTO user_data(username, threshold) VALUES(?,80)", [username]);
                 account.threshold = 80;
+                account.min_payout = 0;
             } else {
                 account.threshold =  data[0].threshold;
+                account.min_payout =  data[0].min_payout;
             }
 
             account.token = access_token;
